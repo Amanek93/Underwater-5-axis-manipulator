695612b173e434a02ee6c91c32c55da20ec743ce
adriandobrzynski <adrian.dobrzynski93@gmail.com>
2021-07-11 16:15:13 +0200

    fixed

--- src/app/auth/screens/login-view.tsx
+++ src/app/auth/screens/login-view.tsx
@@ -1,62 +1,61 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet, View, Image, Text, TouchableOpacity } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
 import i18n from '@shared/language/i18n';
-import { GLOBAL_COLORS,GLOBAL_FONTS, GLOBAL_FONTSIZES } from '@ui';
+import { GLOBAL_COLORS, GLOBAL_FONTS, GLOBAL_FONTSIZES } from '@ui';
 
 import InputText from '@ui/components/InputText';
 import MainButton from '@ui/components/MainButton';
 
-const users = [{
-    name: 'oceantech',
-    password: 'oceantech',
-    role: 'admin',
-},
+const users = [
     {
-    name: 'user',
-    password: 'user',
-    role: 'user',
-}]
-
-
-
+        name: 'oceantech',
+        password: 'oceantech',
+        role: 'admin',
+    },
+    {
+        name: 'user',
+        password: 'user',
+        role: 'user',
+    },
+];
 
 type Props = {
     navigation: StackNavigationProp<any>;
 };
 
 const LoginView = ({ navigation }: Props) => {
-    const [user, setUser] = useState<string>();
-    const [password, setPassword] = useState<string>();
+    const [user, setUser] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
     const [isValidName, setIsValidName] = useState<boolean>(true);
     const [isValidPassword, setIsValidPassword] = useState<boolean>(true);
     const [isValidFoundPassword, setIsValidFoundPassword] = useState<boolean>(true);
     const [found, setFound] = useState<number>(-1);
 
     const handleValidUser = (value: string) => {
-        var findIndex = users.findIndex(x=> x.name === value);
-            if((findIndex != -1) && (value != null)) {
-                if (value === users[findIndex].name) {
-                    setIsValidName(true);
-                    console.log(value);
-                    console.log(findIndex);
-                    setFound(findIndex);
-                } else {
-                    setIsValidName(false);
-                    setFound(findIndex);
-                };
+        const findIndex = users.findIndex(x => x.name === value);
+        if (findIndex !== -1 && value) {
+            if (value === users[findIndex].name) {
+                setIsValidName(true);
+                console.log(value);
+                console.log(findIndex);
+                setFound(findIndex);
             } else {
                 setIsValidName(false);
                 setFound(findIndex);
-            };
+            }
+        } else {
+            setIsValidName(false);
+            setFound(findIndex);
+        }
     };
 
     const handleValidPassword = (value: string) => {
-        if((found != -1) && (value != null)) {
-            var findIndex = users.findIndex(x=> x.name === value);
-            if(found === findIndex){
+        if (found !== -1 && value) {
+            const findIndex = users.findIndex(x => x.name === value);
+            if (found === findIndex) {
                 setIsValidPassword(true);
                 setIsValidFoundPassword(true);
                 console.log(findIndex);
@@ -67,7 +66,7 @@ const LoginView = ({ navigation }: Props) => {
                 setIsValidFoundPassword(true);
                 console.log('cr');
             }
-        } else if(value != null){
+        } else if (value) {
             setIsValidFoundPassword(false);
             console.log(found);
             console.log('b');
@@ -77,130 +76,124 @@ const LoginView = ({ navigation }: Props) => {
     const loginButtonHandler = (myIndex: number, myLogin: string, myPassword: string) => {
         handleValidUser(myLogin);
         handleValidPassword(myPassword);
-        if(myIndex != -1) {
-            if ((users[myIndex].name === myLogin) && (users[myIndex].password === myPassword)) {
+        if (myIndex !== -1) {
+            if (users[myIndex].name === myLogin && users[myIndex].password === myPassword) {
                 console.log('zadzialalo');
                 return navigation.navigate('Welcome');
-            } else console.log('nie zadzialalo')
-        }else console.log('index -1');
-    }
+            } else console.log('nie zadzialalo');
+        } else console.log('index -1');
+    };
 
     return (
         <SafeAreaView style={styles.container}>
-
-                <View style={styles.logoContainer}>
-                    <Image style={styles.logo} source={require('../../../assets/images/ocean-tech-logo.png')}/>
-                </View>
-                <View style={styles.inputTextContainer}>
-                    <InputText
-                        labelValue={user}
-                        bgColor={'white'}
-                        title={'LOGIN'}
-                        onChangeText={(userName: string) => {
-                            setUser(userName);
-                        }}
-
-
-                    />
-                    <View style={styles.textValidationContainer}>
-                    {!isValidName ? (
+            <View style={styles.logoContainer}>
+                <Image
+                    source={require('../../../assets/images/ocean-tech-logo.png')}
+                    style={styles.logo}
+                />
+            </View>
+            <View style={styles.inputTextContainer}>
+                <InputText
+                    bgColor="white"
+                    labelValue={user}
+                    onChangeText={userName => {
+                        setUser(userName);
+                    }}
+                    title={i18n.t('screens.loginView.login')}
+                />
+                <View style={styles.textValidationContainer}>
+                    {!isValidName && (
                         <Text style={styles.validationText}>
-                            Wrong login or email!
+                            {i18n.t('screens.loginView.wrongLogin')}
                         </Text>
-                    ) : null}
-                    </View>
-                    <InputText
-                        labelValue={password}
-                        bgColor={'white'}
-                        title={'PASSWORD'}
-                        onChangeText={(userPassword: string) => {
-                            setPassword(userPassword);
-                        }}
-
-                        secureTextEntry={true}
-                    />
-                    <View style={styles.textValidationContainer}>
-                    {(isValidPassword === false) ? (
-                        <Text style={styles.validationText}>
-                            Wrong password! Check CAPS LOCK!
-                        </Text>
-                    ) : null}
-                    {((isValidFoundPassword === false) && (isValidPassword === true)) ? (
-                        <Text style={styles.validationText}>
-                            Incorrect login or passowrd!
-                        </Text>
-                    ) : null}
-                    </View>
+                    )}
                 </View>
-                <View style={styles.buttonContainer}>
-                    <MainButton title={'REGISTER'} onPress={()=>console.warn('dupa')} style={styles.mainBotton}/>
-                    <MainButton title={'LOGIN'} onPress={()=>loginButtonHandler(found, user, password)} style={styles.mainBotton}/>
+                <InputText
+                    bgColor="white"
+                    labelValue={password}
+                    onChangeText={(userPassword: string) => {
+                        setPassword(userPassword);
+                    }}
+                    secureTextEntry
+                    title={i18n.t('screens.loginView.password')}
+                />
+                <View style={styles.textValidationContainer}>
+                    {!isValidPassword && (
+                        <Text style={styles.validationText}>
+                            {i18n.t('screens.loginView.wrongPassword')}
+                        </Text>
+                    )}
+                    {!isValidFoundPassword && isValidPassword && (
+                        <Text style={styles.validationText}>
+                            {i18n.t('screens.loginView.wrongLoginOrPassword')}
+                        </Text>
+                    )}
                 </View>
+            </View>
+            <View style={styles.buttonContainer}>
+                <MainButton
+                    onPress={() => loginButtonHandler(found, user, password)}
+                    style={styles.mainBotton}
+                    title={i18n.t('screens.loginView.login')}
+                />
+            </View>
             <View style={styles.supportContainer}>
-                <TouchableOpacity style={{backgroundColor: 'white', width: 50, height: 50,}}/>
-                <TouchableOpacity style={{backgroundColor: 'grey', width: 50, height: 50,}}/>
+                <TouchableOpacity style={{ backgroundColor: 'white', width: 50, height: 50 }} />
+                <TouchableOpacity style={{ backgroundColor: 'grey', width: 50, height: 50 }} />
             </View>
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: GLOBAL_COLORS.primary,
-        justifyContent:'center',
-        alignItems: 'center',
-        flex: 1,
-    },
-    text: {
-        color: GLOBAL_COLORS.text,
-        fontSize: GLOBAL_FONTSIZES.header,
-    },
-    logoContainer:{
-        justifyContent:'center',
-        alignItems: 'center',
-        padding: 10,
-    },
-    logo:{
-        justifyContent:'center',
-        alignItems: 'center',
-        backgroundColor: 'transparent',
-    },
-    buttonContainer:{
+    buttonContainer: {
         flexDirection: 'row',
         padding: 10,
     },
-    inputTextContainer:{
-        padding:10,
+    container: {
+        alignItems: 'center',
+        backgroundColor: GLOBAL_COLORS.primary,
+        flex: 1,
+        justifyContent: 'center',
     },
-    textValidationContainer:{
-        width: 300,
-        height: 20,
+    inputTextContainer: {
+        padding: 10,
+    },
+    logo: {
+        alignItems: 'center',
         backgroundColor: 'transparent',
+        justifyContent: 'center',
     },
-    mainBotton:{
-        width: 150,
+    logoContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+    },
+    mainBotton: {
         height: 20,
         marginHorizontal: 20,
-        paddingTop:15,
+        paddingTop: 15,
+        width: 150,
     },
-    validationText:{
+    supportContainer: {
+        bottom: 10,
+        flexDirection: 'row',
+        height: 50,
+        position: 'absolute',
+        right: 100,
+        width: 150,
+    },
+    textValidationContainer: {
+        backgroundColor: 'transparent',
+        height: 20,
+        width: 300,
+    },
+    validationText: {
         color: GLOBAL_COLORS.text,
         fontFamily: GLOBAL_FONTS.ROBOTO,
         fontSize: GLOBAL_FONTSIZES.description,
         letterSpacing: 0.09,
     },
-    supportContainer: {
-        flexDirection: 'row',
-        width: 150,
-        height: 50,
-        position: 'absolute',
-        bottom: 10,
-        right: 100,
-    },
-    supportIcon: {
-        width: 45,
-        height: 45,
-    }
 });
 
 export default LoginView;
--- src/app/ui/components/InputText.tsx
+++ src/app/ui/components/InputText.tsx
@@ -1,75 +1,87 @@
 import React from 'react';
-import {View, SafeAreaView, StyleSheet, Text, TextInput} from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 import { GLOBAL_COLORS, GLOBAL_FONTS, GLOBAL_FONTSIZES } from '../const';
 
 type Props = {
-  labelValue?: string;
-  title?: string;
-  subTitle?: string;
-  placeHolder?: string;
-  bgColor?: string;
+    labelValue?: string;
+    title?: string;
+    subTitle?: string;
+    placeHolder?: string;
+    bgColor?: string;
+    onChangeText?(userName: string): void;
+    secureTextEntry?: boolean;
 };
 
-const InputText = ({labelValue, title, subTitle, placeHolder, bgColor, ...rest}: Props) => {
-
-  return (
-    <SafeAreaView style={styles.mainContainer}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.textContainer}>{title}</Text>
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={[{backgroundColor: `${bgColor}`}, styles.loginInputContainer]}
-          numberOfLines={1}
-          value={labelValue}
-          placeholder={placeHolder}
-          {...rest}
-        />
-      </View>
-      <View style={styles.subTitleContainer}>
-        <Text style={styles.subTextContainer}>{subTitle}</Text>
-      </View>
-    </SafeAreaView>
-  );
+const InputText = ({
+    labelValue,
+    title,
+    subTitle,
+    placeHolder,
+    bgColor,
+    onChangeText,
+    secureTextEntry,
+    ...rest
+}: Props) => {
+    return (
+        <SafeAreaView style={styles.mainContainer}>
+            <View style={styles.titleContainer}>
+                <Text style={styles.textContainer}>{title}</Text>
+            </View>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    numberOfLines={1}
+                    onChangeText={onChangeText}
+                    placeholder={placeHolder}
+                    secureTextEntry={secureTextEntry}
+                    style={[{ backgroundColor: `${bgColor}` }, styles.loginInputContainer]}
+                    value={labelValue}
+                    {...rest}
+                />
+            </View>
+            <View style={styles.subTitleContainer}>
+                <Text style={styles.subTextContainer}>{subTitle}</Text>
+            </View>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  mainContainer: {
-    width: '100%',
-  },
-  titleContainer: {
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    left: 5,
-    paddingVertical: 5,
-  },
-  subTitleContainer: {
-    alignItems: 'flex-end',
-    right: 70,
-  },
-  inputContainer: {
-    alignSelf: 'center',
-  },
-  loginInputContainer: {
-    width: 338,
-    height: 47,
-    borderWidth: 2,
-    borderRadius: 10,
-    borderColor: '#dcdcdc',
-    paddingLeft: 15,
-  },
-  subTextContainer: {
-    color: 'black',
-    fontSize: 11,
-  },
-  textContainer: {
-    color: GLOBAL_COLORS.text,
-    fontFamily: GLOBAL_FONTS.ROBOTO,
-    fontSize: GLOBAL_FONTSIZES.header,
-    fontWeight: 'bold' as const,
-    letterSpacing: 0.09,
-    textAlign: 'center',
-  },
+    inputContainer: {
+        alignSelf: 'center',
+    },
+    loginInputContainer: {
+        borderColor: '#dcdcdc',
+        borderRadius: 10,
+        borderWidth: 2,
+        height: 47,
+        paddingLeft: 15,
+        width: 338,
+    },
+    mainContainer: {
+        width: '100%',
+    },
+    subTextContainer: {
+        color: 'black',
+        fontSize: 11,
+    },
+    subTitleContainer: {
+        alignItems: 'flex-end',
+        right: 70,
+    },
+    textContainer: {
+        color: GLOBAL_COLORS.text,
+        fontFamily: GLOBAL_FONTS.ROBOTO,
+        fontSize: GLOBAL_FONTSIZES.header,
+        fontWeight: 'bold' as const,
+        letterSpacing: 0.09,
+        textAlign: 'center',
+    },
+    titleContainer: {
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        left: 5,
+        paddingVertical: 5,
+    },
 });
 export default InputText;
