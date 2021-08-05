554c5e14eff2f568f05d0cc7d248d5584cf7fd9a
adrian.dobrzynski <adrian.dobrzynski@ithero.pl>
2021-08-05 20:29:51 +0200

    init home stack

--- src/app/shared/navigation/Navigation.tsx
+++ src/app/shared/navigation/Navigation.tsx
@@ -5,13 +5,12 @@ import { createStackNavigator } from '@react-navigation/stack';
 import AuthStack from '@navigation/routes/auth-stack';
 import HomeStack from '@navigation/routes/home-stack';
 
-
 const Stack = createStackNavigator();
 
 const Navigation = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator headerMode="none" initialRouteName="Auth">
+            <Stack.Navigator headerMode="none" initialRouteName="Home">
                 <Stack.Screen component={HomeStack} name="Home" />
                 <Stack.Screen component={AuthStack} name="Auth" />
             </Stack.Navigator>
