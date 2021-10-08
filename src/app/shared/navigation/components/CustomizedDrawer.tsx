15ae31ea90b40611c347c80d70fd48b9ca8f1696
adrian.dobrzynski <adrian.dobrzynski@ithero.pl>
2021-10-09 00:31:44 +0200

    rework navigation bar, add icons

--- src/app/shared/navigation/components/CustomizedDrawer.tsx
+++ src/app/shared/navigation/components/CustomizedDrawer.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useRef, useState } from 'react';
-import i18n from '@shared/language/i18n';
 
 import {
     Animated,
@@ -8,15 +7,23 @@ import {
     Image,
     SafeAreaView,
     StyleSheet,
-    Text,
     TouchableOpacity,
     View,
 } from 'react-native';
 
-import Icon from '@ui/components/Icon';
-import NavigationToggleButton from '@ui/components/NavigationToggleButton';
 import { DrawerNavigationProp } from '@react-navigation/drawer';
-import { GLOBAL_COLORS, GLOBAL_FONTS, GLOBAL_FONTSIZES, GLOBAL_ICONS } from '@ui/const';
+import { GLOBAL_COLORS } from '@ui/const';
+import {
+    arrowIcon,
+    diagnosticIcon,
+    helpIcon,
+    homeIcon,
+    infoIcon,
+    liveCameraIcon,
+    powerIcon,
+    settingsIcon,
+    telemetryIcon,
+} from '../../../../assets/icons';
 import { useNavigation } from '@react-navigation/native';
 
 const DATA: Array<{
@@ -26,79 +33,112 @@ const DATA: Array<{
     navigationId: string;
     isActive: boolean;
     keyId: number;
+    topSeparator: boolean;
+    logoutIcon?: boolean;
+    moreButton?: boolean;
 }> = [
     {
-        icon: GLOBAL_ICONS.home,
+        icon: homeIcon,
         iconColor: GLOBAL_COLORS.icon,
         //title: 'screen.navigationBar.home',
         title: 'home',
         navigationId: 'HomeView',
         isActive: false,
         keyId: 0,
+        topSeparator: true,
     },
     {
-        icon: GLOBAL_ICONS.gamepad,
+        icon: liveCameraIcon,
         iconColor: GLOBAL_COLORS.icon,
         //title: 'screen.test.test',
         title: 'liveStream',
         navigationId: 'LiveStreamView',
         isActive: false,
         keyId: 1,
+        topSeparator: false,
     },
     {
-        icon: GLOBAL_ICONS.cog,
-        iconColor: GLOBAL_COLORS.icon,
-        //title: 'screen.navigationBar.settings',
-        title: 'settings',
-        navigationId: 'SettingsView',
-        isActive: false,
-        keyId: 2,
-    },
-    {
-        icon: GLOBAL_ICONS.telemetry,
+        icon: telemetryIcon,
         iconColor: GLOBAL_COLORS.icon,
         //title: 'screen.navigationBar.telemetry',
         title: 'telemetry',
         navigationId: 'TelemetryView',
         isActive: false,
-        keyId: 3,
+        keyId: 2,
+        topSeparator: false,
     },
     {
-        icon: GLOBAL_ICONS.stethoscope,
+        icon: diagnosticIcon,
         iconColor: GLOBAL_COLORS.icon,
         //title: 'screen.navigationBar.diagnostic',
         title: 'diagnostic',
         navigationId: 'DiagnosticView',
         isActive: false,
-        keyId: 4,
+        keyId: 3,
+        topSeparator: false,
     },
     {
-        icon: GLOBAL_ICONS.question,
+        icon: settingsIcon,
+        iconColor: GLOBAL_COLORS.icon,
+        //title: 'screen.navigationBar.settings',
+        title: 'settings',
+        navigationId: 'SettingsView',
+        isActive: false,
+        keyId: 4,
+        topSeparator: true,
+    },
+    {
+        icon: infoIcon,
         iconColor: GLOBAL_COLORS.icon,
         //title: 'screen.navigationBar.info',
         title: 'info',
         navigationId: 'InfoView',
         isActive: false,
         keyId: 5,
+        topSeparator: false,
     },
     {
-        icon: GLOBAL_ICONS.help,
+        icon: helpIcon,
         iconColor: GLOBAL_COLORS.icon,
         //title: 'screen.navigationBar.help',
         title: 'help',
         navigationId: 'HelpView',
         isActive: false,
         keyId: 6,
+        topSeparator: false,
+    },
+    {
+        icon: powerIcon,
+        iconColor: GLOBAL_COLORS.icon,
+        //title: 'screen.navigationBar.help',
+        title: 'help',
+        navigationId: 'HelpView',
+        isActive: false,
+        keyId: 7,
+        topSeparator: true,
+        logoutIcon: true,
+    },
+    {
+        icon: arrowIcon,
+        iconColor: GLOBAL_COLORS.icon,
+        //title: 'screen.navigationBar.help',
+        title: 'help',
+        navigationId: 'HelpView',
+        isActive: false,
+        keyId: 8,
+        topSeparator: true,
+        moreButton: true,
     },
 ];
 
+const iconsCount = DATA.length;
 const windowHeight = Dimensions.get('window').height;
 
 const CustomizedDrawer = () => {
-    //deklaruję hooka nawigacyjnego
     const navigation = useNavigation<DrawerNavigationProp<any>>();
 
-    const [activeIndex, setIsActive] = useState(0);
+    const [moreOptions, setMoreOptions] = useState<boolean>(false);
+    const [activeIndex, setIsActive] = useState<number>(0);
     const translateX = useRef(new Animated.Value(0)).current;
 
     const show = () => {
@@ -129,153 +169,119 @@ const CustomizedDrawer = () => {
         },
         [activeIndex],
     );
-
-    const renderItem = ({ item, index }: any) => {
+    const renderItem = ({ item }: any) => {
         return (
-            <TouchableOpacity
-                onPress={() => {
-                    //DOPIERO TERAZ MOGĘ Z NIEGO KORZYSTAC
-                    setIsActive(item.keyId);
-
-                    handleButton(item);
-                }}
-                style={
-                    item.keyId === activeIndex
-                        ? styles.pressedButtonContainer
-                        : styles.buttonContainer
-                }
+            <View
+                style={[
+                    styles.itemContainer,
+                    item.topSeparator && styles.separator,
+                    item.logoutIcon && { height: (windowHeight / iconsCount) * 1.2 },
+                ]}
             >
-                <Animated.View
-                    style={[styles.animatedViewContainer, { transform: [{ translateX }] }]}
+                <TouchableOpacity
+                    onPress={() => {
+                        setIsActive(item.keyId);
+                        if (item.moreButton) {
+                            setMoreOptions(true);
+                        } else {
+                            handleButton(item);
+                        }
+                    }}
+                    style={[
+                        { borderRadius: 23 },
+                        item.logoutIcon && styles.logoutButtonContainer,
+                        item.keyId === activeIndex && styles.pressedButtonContainer,
+                    ]}
                 >
-                    {item.keyId === activeIndex && (
-                        <View style={{ backgroundColor: 'grey', flex: 1 }} />
+                    {item.keyId === activeIndex ? (
+                        <View style={styles.iconContainer}>
+                            <Image
+                                resizeMode="contain"
+                                source={item.icon}
+                                style={styles.iconCont}
+                            />
+                        </View>
+                    ) : (
+                        <View style={styles.iconContainer}>
+                            <Image
+                                resizeMode="contain"
+                                source={item.icon}
+                                style={styles.iconCont}
+                            />
+                        </View>
                     )}
-                </Animated.View>
-                {item.keyId === activeIndex ? (
-                    <Animated.View
-                        style={[styles.flatListButtonContainer, { transform: [{ translateX }] }]}
-                    >
-                        <View style={styles.iconContainer}>
-                            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                                <Icon
-                                    color={item.iconColor}
-                                    name={item.icon}
-                                    size={42}
-                                    style={styles.iconCont}
-                                />
-                            </View>
-                        </View>
-                        <View style={styles.flatListTextContainer}>
-                            <Text style={styles.text}>
-                                {i18n.t(`screens.navigationBar.${item.title}`)}
-                            </Text>
-                        </View>
-                    </Animated.View>
-                ) : (
-                    <View style={styles.flatListButtonContainer}>
-                        <View style={styles.iconContainer}>
-                            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                                <Icon
-                                    color={item.iconColor}
-                                    name={item.icon}
-                                    size={42}
-                                    style={styles.iconCont}
-                                />
-                            </View>
-                        </View>
-                        <View style={styles.flatListTextContainer}>
-                            <Text style={styles.text}>
-                                {i18n.t(`screens.navigationBar.${item.title}`)}
-                            </Text>
-                        </View>
-                    </View>
-                )}
-            </TouchableOpacity>
+                </TouchableOpacity>
+            </View>
         );
     };
     return (
         <SafeAreaView style={styles.statusBarContainer}>
-            <View
-                style={{ height: windowHeight / 7, justifyContent: 'center', alignItems: 'center' }}
-            >
+            <View style={styles.logoContainer}>
                 <Image
                     resizeMode="contain"
                     source={require('../../../../assets/images/ocean-tech-logo.png')}
                     style={styles.imageContainer}
                 />
             </View>
-
             <FlatList
                 data={DATA}
                 keyExtractor={item => item.keyId.toString()}
                 numColumns={1}
                 renderItem={renderItem}
             />
-            <NavigationToggleButton onPress={() => {}} />
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
-    animatedViewContainer: {
-        backgroundColor: GLOBAL_COLORS.secondary,
-        height: '100%',
-        right: 15,
-        width: 15,
-    },
-    buttonContainer: {
-        backgroundColor: GLOBAL_COLORS.secondary,
-        flexDirection: 'row',
-        width: 160,
-    },
-    flatListButtonContainer: {
-        flex: 1,
-        height: (windowHeight - windowHeight / 7) / 7,
-        right: 5,
-        top: 10,
-    },
-
-    flatListTextContainer: {
-        alignItems: 'center',
-        flex: 0.5,
-        justifyContent: 'flex-start',
-    },
     iconCont: {
         alignItems: 'center',
-        flex: 0.5,
         justifyContent: 'center',
     },
     iconContainer: {
         alignItems: 'center',
-        flex: 0.5,
+        alignSelf: 'center',
+        height: (windowHeight - 370) / iconsCount,
         justifyContent: 'center',
+        width: (windowHeight - 370) / iconsCount,
     },
-
     imageContainer: {
         alignItems: 'center',
         justifyContent: 'center',
-        width: 175,
+        maxHeight: 42,
+        maxWidth: 83,
+    },
+    itemContainer: {
+        backgroundColor: 'rgba(3, 26, 66, 1)',
+        height: (windowHeight - 220) / iconsCount,
+        justifyContent: 'center',
+        width: (windowHeight - 220) / iconsCount,
+    },
+    logoContainer: {
+        alignItems: 'center',
+        height: windowHeight / 7,
+        justifyContent: 'center',
+    },
+    logoutButtonContainer: {
+        backgroundColor: 'rgba(212, 215, 223, 0.35)',
+        borderRadius: 42,
+        height: (windowHeight - 100) / iconsCount,
+        justifyContent: 'center',
     },
     pressedButtonContainer: {
-        backgroundColor: GLOBAL_COLORS.extra,
-        flexDirection: 'row',
-        width: 175,
+        backgroundColor: 'rgba(5, 120, 227, 0.4)',
+    },
+    separator: {
+        borderTopColor: 'white',
+        borderTopWidth: 2,
     },
     statusBarContainer: {
         alignItems: 'center',
-        backgroundColor: GLOBAL_COLORS.secondary,
+        backgroundColor: 'rgba(3, 26, 66, 1)',
+        borderRadius: 55,
         height: '100%',
         justifyContent: 'center',
-    },
-    text: {
-        color: GLOBAL_COLORS.text,
-        fontFamily: GLOBAL_FONTS.ROBOTO,
-        fontSize: GLOBAL_FONTSIZES.header,
-        fontWeight: 'bold' as const,
-        letterSpacing: 0.09,
-        textAlign: 'center',
-        width: 140,
+        width: 128,
     },
 });
 
