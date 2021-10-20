5df508af2ab0498365e34c7cc9c2310a0cec6f60
adrian.dobrzynski <adrian.dobrzynski@ithero.pl>
2021-10-20 23:18:05 +0200

    add reference

--- src/app/liveStream/components/JoystickPad.tsx
+++ src/app/liveStream/components/JoystickPad.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from 'react';
-import {Animated, Easing, StyleSheet, StyleProp, ViewStyle} from 'react-native';
-
+import { Animated, Easing, StyleProp, StyleSheet, ViewStyle } from 'react-native';
 
 type Props = {
     widthSize?: number;
@@ -13,7 +12,7 @@ type Props = {
     resetOnRelease?: boolean;
     axisPad?: StyleProp<ViewStyle>;
     axisPadContainer?: StyleProp<ViewStyle>;
-}
+};
 
 const JoystickPad = ({
     widthSize,
@@ -25,7 +24,7 @@ const JoystickPad = ({
     lockY,
     resetOnRelease,
     axisPad,
-    axisPadContainer
+    axisPadContainer,
 }: Props) => {
     const [identifier, setIdentifier] = useState<number>(0);
     const [cx, setCx] = useState<number>(0);
@@ -41,6 +40,9 @@ const JoystickPad = ({
     const handler = handlerSize ? handlerSize : 100;
     const step = stepRate ? stepRate : 0;
 
+    const handlerElement = useRef(null);
+    const wrappedElement = useRef(null);
+
     const animCx = useRef(new Animated.Value(0)).current;
     const animCy = useRef(new Animated.Value(0)).current;
     const animPx = useRef(new Animated.Value(0)).current;
@@ -51,9 +53,9 @@ const JoystickPad = ({
             toValue: cx,
             duration: 300,
             easing: Easing.elastic(1),
-            useNativeDriver: true
+            useNativeDriver: true,
         }).start();
-        Animated.timing(animCy,{
+        Animated.timing(animCy, {
             toValue: cy,
             duration: 300,
             easing: Easing.elastic(1),
@@ -68,7 +70,7 @@ const JoystickPad = ({
             easing: Easing.out(Easing.exp),
             useNativeDriver: true,
         }).start();
-        Animated.timing(animPy,{
+        Animated.timing(animPy, {
             toValue: py,
             duration: 50,
             easing: Easing.out(Easing.exp),
@@ -76,42 +78,44 @@ const JoystickPad = ({
         }).start();
     };
 
-    const limiter = ( input: number ) => {
-        const minimised = input / width * 2;
-        const stepped = (x: number) => step ? Math.floor(x / step) * step : x;
+    const limiter = (input: number) => {
+        const minimised = (input / width) * 2;
+        const stepped = (x: number) => (step ? Math.floor(x / step) * step : x);
         const limited = (x: number) => Math.min(1, Math.max(-1, x));
-        return stepped(limited(minimised)) * width / 2;
+        return (stepped(limited(minimised)) * width) / 2;
     };
 
-    const sendValue = ( x: number, y: number ) => {
+    const sendValue = (x: number, y: number) => {
         //nie do końca rozumiem tę formułę
-        onValue && onValue( x / width * 2, y / width * 2)
+        onValue && onValue((x / width) * 2, (y / width) * 2);
     };
 
-    const centerPosition = ( pageX: number, pageY: number) => {
+    const centerPosition = (pageX: number, pageY: number) => {
         //Gosc to deklarowal przez ref={} w widoku <Animated.View>. Nie wiem jak to zmienić
-       handlerElement.getNode().measure(( width: number, px: number, py: number) => {
-            setCx((pageX - px - width / 2));
-            setCy((pageY - py - width / 2));
+        handlerElement?.current.getNode().measure((width: number, px: number, py: number) => {
+            setCx(pageX - px - width / 2);
+            setCy(pageY - py - width / 2);
         }, centerAnimate());
     };
 
-    const setPositon = ( pageX: number, pageY: number, after: any) => {
+    const setPositon = (pageX: number, pageY: number, after: any) => {
         //Gosc to deklarowal przez ref={} w widoku <Animated.View>. Nie wiem jak to zmienić
-        wrappedElement.getNode().measure(( width: number, height: number, px: number, py: number) => {
-            const cx = px + width / 2;
-            const cy = py + height / 2;
-            setSx((pageX - px - width / 2));
-            setSy((pageY - py - width / 2));
-            setPx((pageX - cx));
-            setPy((pageY - cy));
-        }, after);
+        wrappedElement?.current
+            .getNode()
+            .measure((width: number, height: number, px: number, py: number) => {
+                const cx = px + width / 2;
+                const cy = py + height / 2;
+                setSx(pageX - px - width / 2);
+                setSy(pageY - py - width / 2);
+                setPx(pageX - cx);
+                setPy(pageY - cy);
+            }, after);
     };
 
-    const getTouchPoint = ( touches: any, identifier: number) => {
+    const getTouchPoint = (touches: any, identifier: number) => {
         let touchItem = null;
         touches.map((item: any) => {
-            if(item.identifier === identifier) {
+            if (item.identifier === identifier) {
                 touchItem = item;
             }
         });
@@ -121,12 +125,11 @@ const JoystickPad = ({
     const onTouchStart = (evt: any) => {
         const identifier = evt.nativeEvent.identifier;
         const touchItem = getTouchPoint(evt.nativeEvent.touches, identifier);
-        console.log(evt.nativeEvent.touches, identifier, touchItem);
 
-        if (typeof identifier === "number" && touchItem) {
-            const {pageX, pageY} = touchItem;
-            if(autoCenter) {
-                centerPosition(pageX, pageY)
+        if (typeof identifier === 'number' && touchItem) {
+            const { pageX, pageY } = touchItem;
+            if (autoCenter) {
+                centerPosition(pageX, pageY);
                 sendValue(px, py);
                 setIdentifier(identifier);
                 setSx(pageX);
@@ -135,9 +138,8 @@ const JoystickPad = ({
                 setPositon(pageX, pageY, () => {
                     sendValue(px, py);
                     animate();
-                })
+                });
             }
-
         }
     };
 
@@ -145,7 +147,7 @@ const JoystickPad = ({
         const touchItem = getTouchPoint(evt.nativeEvent.touches, identifier);
 
         if (touchItem) {
-            const {pageX, pageY} = touchItem;
+            const { pageX, pageY } = touchItem;
 
             let px = lockX ? 0 : pageX - sx + dx;
             let py = lockY ? 0 : pageY - sy + dy;
@@ -157,16 +159,16 @@ const JoystickPad = ({
             setPy(py);
             animate();
         }
-    }
+    };
 
     const onTouchEnd = () => {
-        if (resetOnRelease){
+        if (resetOnRelease) {
             //px = 0;
             //py = 0;
             setPx(0);
             setPy(0);
         }
-        if (autoCenter){
+        if (autoCenter) {
             //dx = px;
             //dy = py;
             setDx(px);
@@ -186,72 +188,83 @@ const JoystickPad = ({
         setDy(dy);
         centerAnimate();
         animate();
-    }
+    };
 
     const onTouchCancel = () => {
         setCx(0);
         setCy(0);
         centerAnimate();
-    }
+    };
 
-    return(
+    return (
         <Animated.View
-            onTouchStart={onTouchStart}
-            onTouchEnd={onTouchEnd}
             onTouchCancel={onTouchCancel}
+            onTouchEnd={onTouchEnd}
             onTouchMove={onTouchMove}
-            style={[styles.AxisPadContainer, axisPadContainer ? axisPadContainer : {}, {
-                width: handler,
-                height: handler,
-                transform:[{
-                    translateX: animCx,
-                }, {
-                    translateY: animCy,
-                }]
-            }]}
-            //ref={view => { this.wrapperElement = view; }}  <- tak to deklarowal
-        >
-
-            <Animated.View
-                style={[styles.AxisPad, axisPad ? axisPad: {}, {
+            onTouchStart={onTouchStart}
+            ref={wrappedElement}
+            style={[
+                styles.AxisPadContainer,
+                axisPadContainer ? axisPadContainer : {},
+                {
                     width: handler,
                     height: handler,
-                    transform: [{
-                        translateX: animPx,
-                    }, {
-                        translateY: animPy,
-                    }]
-                }]}
-            >
-
-            </Animated.View>
-
+                    transform: [
+                        {
+                            translateX: animCx,
+                        },
+                        {
+                            translateY: animCy,
+                        },
+                    ],
+                },
+            ]}
+        >
+            <Animated.View
+                ref={handlerElement}
+                style={[
+                    styles.AxisPad,
+                    axisPad ? axisPad : {},
+                    {
+                        width: handler,
+                        height: handler,
+                        transform: [
+                            {
+                                translateX: animPx,
+                            },
+                            {
+                                translateY: animPy,
+                            },
+                        ],
+                    },
+                ]}
+            />
         </Animated.View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        width: 500,
-        height: 500,
-        backgroundColor: 'red',
+    AxisPad: {
+        alignItems: 'center',
+        backgroundColor: '#00000066',
+        borderRadius: 300,
+        height: '60%',
+        justifyContent: 'center',
+        width: '60%',
     },
     AxisPadContainer: {
-        width: 300,
-        height: 300,
-        borderRadius: 300,
-        justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#00000033',
-    },
-    AxisPad: {
-        width: "60%",
-        height: "60%",
-        justifyContent: 'center',
-        alignItems: 'center',
         borderRadius: 300,
-        backgroundColor: '#00000066'
-    }
-})
+        height: 300,
+        justifyContent: 'center',
+        width: 300,
+    },
+    mainContainer: {
+        backgroundColor: 'red',
+        height: 500,
+        width: 500,
+    },
+});
 
 export default JoystickPad;
